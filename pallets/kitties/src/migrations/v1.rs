c6ef91efe3d99537a37c8c4f1aa85eafd03b1e41
LiZhuofu <2414453629@qq.com>
2023-06-11 23:25:04 +0800

    完成第三课作业

--- pallets/kitties/src/migrations/v1.rs
+++ pallets/kitties/src/migrations/v1.rs
@@ -1,16 +1,42 @@
-use super::v0;
-use codec::{Decode, Encode, MaxEncodedLen};
-use frame_support::{pallet_prelude::*, traits::StorageVersion};
-use scale_info::TypeInfo;
+// use frame_support::{
+//     pallet_prelude::*,
+//     storage::StoragePrefixedMap,
+//     traits::GetStorageVersion,
+//     weights::Weight,
+//     migration::storage_key_iter,
+//     Blake2_128Concat,
+// };
 
-pub(crate) const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+// use crate::{Config, Pallet, Kitty, KittyId, Kitties};
 
-pub type KittyId = v0::KittyId;
-pub type KittyDna = v0::KittyDna;
-pub type KittyName = [u8; 4];
 
-#[derive(Clone, PartialEq, Eq, Default, TypeInfo, Encode, Decode, MaxEncodedLen, RuntimeDebug)]
-pub struct Kitty {
-	pub name: KittyName,
-	pub dna: KittyDna,
-}
+// #[derive(Encode, Decode, Clone, Debug, TypeInfo, MaxEncodedLen, PartialEq, Eq)]
+// pub struct OldKitty(pub [u8; 16]);
+
+// pub fn migrate<T: Config>() -> Weight {
+//     let on_chain_storage_version = Pallet::<T>::on_chain_storage_version();
+//     let current_version: StorageVersion = Pallet::<T>::current_storage_version();
+// 	log::info!("### on_chain_storage_version={:?}, current_version={:?}", on_chain_storage_version, current_version);
+
+//     if on_chain_storage_version != 0 {
+//         return Weight::zero();
+//     }
+
+//     if current_version != 1 {
+//         return Weight::zero();
+//     }
+
+//     let module = Kitties::<T>::module_prefix();
+//     let item = Kitties::<T>::storage_prefix();
+
+//     for(index,kitty) in storage_key_iter::<KittyId, OldKitty,Blake2_128Concat>(module, item).drain(){
+  
+//         let new_kitty = Kitty{
+//             dna: kitty.0,
+//             name: *b"abcd",
+//         };
+
+//         Kitties::<T>::insert(index, &new_kitty);
+//     }
+//     Weight::zero()
+// }
